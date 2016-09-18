7118529626cb5479758f692f866dfed2294f82b3
josoroma <pablo@josoroma.com>
2016-09-17 23:37:56 -0600

    Improves ninjadevsio.php indentation

--- ninjadevs.io/wp-content/plugins/ninjadevsio/ninjadevsio.php
+++ ninjadevs.io/wp-content/plugins/ninjadevsio/ninjadevsio.php
@@ -54,18 +54,18 @@ add_filter('bp_ajax_querystring', 'ninjadevsio_bp_per_page', 9999, 2);
 function ninjadevsio_bp_exclude_users($qs=false, $object=false) {
     $excluded_user = '1';
 
-    if( $object != 'members' )
-    return $qs;
+    if ($object != 'members')
+        return $qs;
 
     $args = wp_parse_args($qs);
 
-    if( !empty($args['user_id']) || !empty($args['search_terms']) )
-    return $qs;
+    if (!empty($args['user_id']) || !empty($args['search_terms']))
+        return $qs;
 
-    if ( !empty($args['exclude']) )
-    $args['exclude'] = $args['exclude'] . ',' . $excluded_user;
+    if (!empty($args['exclude']))
+        $args['exclude'] = $args['exclude'] . ',' . $excluded_user;
     else
-    $args['exclude'] = $excluded_user;
+        $args['exclude'] = $excluded_user;
 
     $qs = build_query($args);
 
