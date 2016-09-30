fe172765ca9011fe85db7ec6f5065516fc04a2d3
josoroma <pablo@josoroma.com>
2016-09-29 18:50:19 -0600

    Removes a bunch of wp_meta_boxes

--- ninjadevs.io/wp-content/plugins/ninjadevsio/ninjadevsio.php
+++ ninjadevs.io/wp-content/plugins/ninjadevsio/ninjadevsio.php
@@ -364,13 +364,12 @@ function ninjadevsio_remove_dashboard_widgets() {
 
 	unset($wp_meta_boxes['dashboard']['side']['core']['dashboard_quick_press']);
 	unset($wp_meta_boxes['dashboard']['normal']['core']['dashboard_incoming_links']);
-	unset($wp_meta_boxes['dashboard']['normal']['core']['dashboard_right_now']);
+	// unset($wp_meta_boxes['dashboard']['normal']['core']['dashboard_right_now']);
 	unset($wp_meta_boxes['dashboard']['normal']['core']['dashboard_plugins']);
 	// unset($wp_meta_boxes['dashboard']['normal']['core']['dashboard_recent_drafts']);
 	// unset($wp_meta_boxes['dashboard']['normal']['core']['dashboard_recent_comments']);
 	unset($wp_meta_boxes['dashboard']['side']['core']['dashboard_primary']);
-	unset($wp_meta_boxes['dashboard']['side']['core']['dashboard_secondary']);
-
+	// unset($wp_meta_boxes['dashboard']['side']['core']['dashboard_secondary']);
 }
 
 add_action('wp_dashboard_setup', 'ninjadevsio_remove_dashboard_widgets' );
