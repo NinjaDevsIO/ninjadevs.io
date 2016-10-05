055b04ef69e5491b5c82a2970014735f0c7f0542
josoroma <pablo@josoroma.com>
2016-10-05 04:02:07 -0600

    Adds meta data to posts and a bunch of global social links

--- ninjadevs.io/wp-content/themes/ninjadevs/templates/content-single.php
+++ ninjadevs.io/wp-content/themes/ninjadevs/templates/content-single.php
@@ -10,6 +10,17 @@
       <h1 class="entry-title"><?php the_title(); ?></h1>
       <h3 class="entry-title-decoration"><?=__('by', 'sage') ?></h3>
       <h2 class="entry-title-author"><a href="<?= get_author_posts_url(get_the_author_meta('ID')) ?>"><?= get_the_author() ?></a></h2>
+
+      <div class="row">
+        <div class="col-md-4"></div>
+        <div class="col-md-4 meta-card">
+          <small>
+            <?php get_template_part('templates/entry-meta'); ?>
+          </small>
+        </div>
+        <div class="col-md-4"></div>
+      </div>
+
     </header>
 
     <div class="entry-content">
--- ninjadevs.io/wp-content/themes/ninjadevs/templates/footer.php
+++ ninjadevs.io/wp-content/themes/ninjadevs/templates/footer.php
@@ -7,6 +7,26 @@
 
     </div><!--/row-->
 
+    <div class="row">
+      <div class="col-md-3"></div>
+      <div class="col-md-6 footer-card">
+        <small>
+          <a href="/" title="WordPress">NinjaDevs&copy;2016</a>
+          &nbsp;-&nbsp;
+          <a href="https://www.facebook.com/ninjadevsio" title="Facebook Page">facebook</a>
+          &nbsp;-&nbsp;
+          <a href="https://www.facebook.com/groups/ninjadevsio" title="Facebook Group">group</a>
+          &nbsp;-&nbsp;
+          <a href="https://twitter.com/ninjadevsio" title="Twitter">twitter</a>
+          &nbsp;-&nbsp;
+          <a href="https://www.instagram.com/ninjadevs.io" title="Instagram">instagram</a>
+          &nbsp;-&nbsp;
+          <a href="https://github.com/NinjaDevsIO" title="Github">github</a>
+        </small>
+      </div>
+      <div class="col-md-3"></div>
+    </div>
+
     <div class="footer-img">
       <img src="<?= get_template_directory_uri() ?>/assets/images/footer-img.png">
     </div>
